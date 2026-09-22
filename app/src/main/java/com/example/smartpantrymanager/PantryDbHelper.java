7ac3710161a72034008e7e434c764db64c3346a2
Cebo N <nomcebomkhoma@gmail.com>
2026-09-23 01:10:28 +0200

    Day 4: Added SQLite database helper for pantry items

---
+++ app/src/main/java/com/example/smartpantrymanager/PantryDbHelper.java
@@ -0,0 +1,10 @@
+package com.example.smartpantrymanager;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class PantryDbHelper extends SQLiteOpenHelper {
+    
+}
