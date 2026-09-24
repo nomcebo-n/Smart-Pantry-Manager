c44c571cd057e35f6d7e4054add74989ee190bd1
Cebo N <nomcebomkhoma@gmail.com>
2026-09-24 23:09:02 +0200

    Connected RecyclerView in PantryActivity to PantryAdapter and database

--- app/src/main/java/com/example/smartpantrymanager/PantryActivity.java
+++ app/src/main/java/com/example/smartpantrymanager/PantryActivity.java
@@ -1,24 +1,28 @@
 package com.example.smartpantrymanager;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.database.Cursor;
+import androidx.recyclerview.widget.RecyclerView;
+
 
 public class PantryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_pantry);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+        //will find the RecyclerView in the layout
+        RecyclerView= findViewById(R.id.pantryRecyclerView);
+
+        //will get data from the database
+        PantryRepository repo = new PantryRepository(this);
+        Cursor cursor = repo.getAllItems();
+
+        //will connect to adapter to recycleview
+        PantryAdapter = new PantryAdapter(this,cursor);
+
     }
 }
